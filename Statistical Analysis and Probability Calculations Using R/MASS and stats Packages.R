8afca5bc5f644c7ed63a28aadb5c732de7943653
RimaZ1597 <146136442+RimaZ1597@users.noreply.github.com>
2024-10-21 19:18:21 -0400

    Update MASS and stats Packages.R

--- Statistical Analysis and Probability Calculations Using R/MASS and stats Packages.R
+++ Statistical Analysis and Probability Calculations Using R/MASS and stats Packages.R
@@ -1,1 +1,13 @@
+rm(list=ls())
+#1
+#A n=20 p=1/4
 
+#B
+prob_less_than_8 <- pbinom(7, size = 20, prob = 0.25, lower.tail = TRUE)
+prob_less_than_8
+prob_8_or_more <- 1 - prob_less_than_8
+prob_8_or_more
+
+#C
+prob_exactly_4 <- dbinom(4, size = 20, prob = 0.25)
+prob_exactly_4
